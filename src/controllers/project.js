5da0f2f7ab25974435cd0cb9c26cdaeb4616e0a8
sora100fuegos <cienfuegossilvacarlos@gmail.com>
2023-05-19 20:14:15 -0600

    loas testing example added

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -62,8 +62,8 @@ exports.updateProject = async (req, res) => {
     try {
     const  { id } = req.params;
 
-      const  updatedproject  = await ProjectService.updateProject(id,req.body);
-      res.status(200).json(updatedProject);
+     await ProjectService.updateProject(id,req.body);
+      res.status(200).json();
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Internal error" });
