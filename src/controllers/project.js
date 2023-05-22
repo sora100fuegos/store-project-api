4b96c62f2d89086f1b6279dfb4eefa0f606b97db
sora100fuegos <cienfuegossilvacarlos@gmail.com>
2023-05-22 12:11:20 -0600

    delete project typo corrected

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -62,8 +62,8 @@ exports.updateProject = async (req, res) => {
     try {
     const  { id } = req.params;
 
-     await ProjectService.updateProject(id,req.body);
-      res.status(200).json();
+     await ProjectService.deleteProject(id,req.body);
+      res.status(204).json();
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Internal error" });
